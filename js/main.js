166a67439b72ddb655bcc8057bc9cf13e81c1d71
NullVektor1 <frozendosen@gmail.com>
2022-01-21 14:30:42 +0200

    rollover system created,wall is placeable

--- js/main.js
+++ js/main.js
@@ -1,21 +1,62 @@
+//Set Globals
+var scene,camera,renderer,canvas,board,cameraController;
 
 
+init();
+
+function init(){
+    createScene();
+    setLights();
+    addCubes();
+    console.log(scene);
+    animate();
+}
+
+
+
+
+function createScene(){
 //Canvas 
-const canvas = document.querySelector('canvas.webgl')
+ canvas = document.querySelector('canvas.webgl')
 //Creating the scene
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({
+ scene = new THREE.Scene();
+ camera = new THREE.PerspectiveCamera(85, window.innerWidth / window.innerHeight, 0.1, 1000);
+ renderer = new THREE.WebGLRenderer({
     canvas: canvas
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 //document.body.appendChild( renderer.domElement );
+camera.position.z = 100;
+camera.position.y = -5;
+camera.position.x = 0;
+scene.add(camera);
 cameraController = new THREE.OrbitControls(camera, canvas);
+//controls.enableDamping = true
 
-//Create the board
-const squareGeom = new THREE.PlaneGeometry(1, 1);
+}
 
-const board = [
+function setLights(){
+const pointLight = new THREE.PointLight(0xffffff, 0.1)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
+}
+
+function addCubes() {
+
+    var xDistance = 7.5;
+    var yDistance = 7.5;
+    var geometry = new THREE.BoxGeometry(10,10,10);
+    var colWallGeom = new THREE.BoxGeometry(10,5,5);
+    var rowWallGeom = new THREE.BoxGeometry(5,10,5)
+    var material = new THREE.MeshBasicMaterial({color:0xcf9b3c});
+    var wallMaterial = new THREE.MeshBasicMaterial({ color: 0x694404 });
+
+
+    //Create the board
+
+ board = [
 
     [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -36,54 +77,29 @@ const board = [
     [2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2]
 
 ]
-
-
-
-
-//Objects
-const geometry = new THREE.BoxGeometry();
-
-//materials
-const squareMaterial = new THREE.MeshBasicMaterial({ color: 0xcf9b3c });
-const wallMaterial = new THREE.MeshBasicMaterial({ color: 0x001ff0 });
-
-
-//set camera position
-
-camera.position.x =100;
-
-console.log(board.length);
-addCubes();
-function addCubes() {
-    var xDistance = 7.5;
-    var yDistance = 7.5;
-    var geometry = new THREE.BoxGeometry(12,12,10);
-    var colWallGeom = new THREE.BoxGeometry(10,5,5);
-    var rowWallGeom = new THREE.BoxGeometry(5,10,5)
-    var material = new THREE.MeshBasicMaterial({color:0xcf9b3c});
-
     //initial offset so does not start in middle.
-    var xOffset = -10;
+    var xOffset = -65;
+    var yOffset = -40;
 
-    //CreateTheMap
+    //CreateTheMapOnScene
     for(var i = 0; i < board.length; i++){
          for(var j = 0; j < board.length; j++){
              if(board[i][j]==2){
                  var mesh  = new THREE.Mesh(geometry, material);
 
                  mesh.position.x = (xDistance * i) + xOffset;
-                 mesh.position.y = (yDistance * j);
+                 mesh.position.y = (yDistance * j) + yOffset;
                  
              }else if(board[i][j]==1 && (i%2 == 0 || i == 0)){
                 var mesh  = new THREE.Mesh(colWallGeom, wallMaterial);
                 mesh.position.x = (xDistance * i) + xOffset;
-                mesh.position.y = (yDistance * j);
+                mesh.position.y = (yDistance * j) + yOffset;
                 mesh.position.z = (-2.5 );
                 console.log(mesh)
              }else if(board[i][j]==1 && (i%2 == 1)){
                 var mesh  = new THREE.Mesh(rowWallGeom, wallMaterial);
                 mesh.position.x = (xDistance * i) + xOffset;
-                mesh.position.y = (yDistance * j);
+                mesh.position.y = (yDistance * j) + yOffset;
                 mesh.position.z = (-2.5 );
 
              }
@@ -92,16 +108,8 @@ function addCubes() {
     };
 }
 
-
-camera.position.z = 200;
-
-
-
-animate();
-
-
-
 function animate() {
     requestAnimationFrame(animate);
+   // camera.position.x+=0.5;
     renderer.render(scene, camera);
 }
